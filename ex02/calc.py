97fb305975042edff6eb4b69180db336b85e0e29
uchida keiju <c0a21018b8@edu.teu.ac.jp>
2022-06-21 17:08:16 +0900

    calc enshu

--- ex02/calc.py
+++ ex02/calc.py
@@ -7,32 +7,38 @@ def button_click(event):
     if num == "=":
         eqn = entry.get()
         res = eval(eqn)
-        entry.delate(0, tk.END)
+        entry.delete(0, tk.END)
         entry.insert(tk.END, res)
+    elif num == 'C':
+        entry.delete(0, tk.END)
+    elif num == 'B':
+        pos_end_prev = len(entry.get())-1
+        entry.delete(pos_end_prev, tk.END)
     else:    
         #tkm.showinfo("", f"{num}のボタンがクリックされました")
         entry.insert(tk.END, num)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("calc")
+    root.title("電卓")
     #root.geometry("300x500")
 
     entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 40))
-    entry.grid(row=0, column=0, columnspan=3)
+    entry.grid(row=0, column=0, columnspan=4, sticky=tk.EW)
 
-    r, c = 0, 0    #r:行番号 c:列番号
-    for i,num in enumerate([i for i in range(9, -1, -1)]+["+"]+["="]):
+    r, c = 1, 0    #r:行番号 c:列番号
+    for i,num in enumerate(['', 'B', 'C', '/', 7, 8, 9, '*', 4, 5, 6,
+                          "-", 1, 2, 3, "+", '00', 0, '.', '=']):
         button = tk.Button(root,
                           text=f"{num}",
-                          width=4,
-                          height=2,
+                          width=3,
+                          height=1,
                           font=("Times New Roman", 30)
                         )
         button.bind("<1>", button_click)
         button.grid(row=r, column=c)
         c += 1
-        if (i+1)%3 == 0:
+        if (i+1)%4 == 0:
             r += 1
             c = 0
 
